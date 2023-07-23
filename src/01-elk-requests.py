a25292c6aa98a50c149d5f709f1dbdb9499667b1
Bob Kozdemba <bkozdemba@gmail.com>
2023-07-23 01:33:12 +0000

    updated

--- src/01-elk-requests.py
+++ src/01-elk-requests.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Example Elastic client using requests.
+"""
+Example Elastic client using requests.
+"""
 
 import requests
 import json
 import argparse
-# ##### Authenticate with the Elastic service. Fill in the `service`, `user` and `password`.
 
-# ##### GET the Elastic health
-def get_health(session: requests.Session, url: str, port: str, insecure: bool):
+def get_health(session: requests.Session, url: str, port: str, verify: bool):
     """
     get_health
     """
-    # url = f'http://{service}:9200/_cat/health'
-    r = session.get(f'{url}:{port}', verify=insecure)
+    r = session.get(f'{url}:{port}/_cat/health', verify=verify)
+    print(r.content.decode())
+    r = session.get(f'{url}:{port}', verify=verify)
     print(r.content.decode())
 
 
-# ##### GET the Elastic status.
-# url = f'http://{service}:9200'
-# r = session.get(url, verify=verify_certs)
-# print(r.content.decode())
-
-
-# ##### POST some example data.
+# POST some example data.
 # url = f'http://{service}:9200/customer/_doc/4'
 # payload = {"firstname": "Bob", "lastname": "Koz"}
 # r = session.put(url, verify=verify_certs, json=payload)
@@ -41,7 +36,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--host', metavar='host', nargs='?',
-        default='http://localhost',
+        default='https://localhost',
         help='Hostname of the ElasticSearch cluster'
         )
     parser.add_argument(
@@ -66,11 +61,11 @@ if __name__ == "__main__":
     #    action='store_true',
     #    help='Verify checking of SSL certs.'
     #    )
-    parser.add_argument('-i', '--insecure', action='store_false', default=True)
+    parser.add_argument('-i', '--verify', action='store_true', default=False)
 
     args = parser.parse_args()
     # if args.verify_certs == "False": args.verify_certs = False
-    print(f'host = {args.host}, port = {args.port}, user = {args.user}, password = {args.password}, insecure = {args.insecure}')
+    print(f'host = {args.host}, port = {args.port}, user = {args.user}, password = {args.password}, verify = {args.verify}')
 
 
     # service = 'elasticsearch-sample-es-http.elastic'
@@ -80,4 +75,4 @@ if __name__ == "__main__":
     session = requests.Session()
     session.auth = (args.user, args.password)
     
-    get_health(session, args.host, args.port, args.insecure)
+    get_health(session, args.host, args.port, args.verify)
